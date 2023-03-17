c805e0749ed0bf6685471682260ceebeb8f246d9
Adrian-Oliveira <adrianalves483@gmail.com>
2023-03-17 17:36:47 -0300

    Layout finished

--- src/App.tsx
+++ src/App.tsx
@@ -9,19 +9,92 @@ function App() {
     <div className="App">
       <h1>Input</h1>
 
+      <div className='row'>
+        <div className="element">
+          <p>{'<Input/>'}</p>
+          <Input/>
+        </div>
+      </div>
 
-      <Input/>
-      <Input error/>
-      <Input disabled/>
-      <Input helperText="Some interesting text" />
-      <Input helperText="Some interesting text" error/>
-      <Input startIcon='email'/> 
-      <Input endIcon='call'/> 
-      <Input value='text' />
-      <Input size='sm' />
-      <Input size='md' />
-      <Input fullWidth />
-      <Input rows={4} />
+      <div className='row'>
+        <div className="element">
+          <p>{'<Input error/>'}</p>
+          <Input error/>
+        </div>
+      </div>
+
+
+      <div className='row'>
+        <div className="element">
+          <p>{'<Input disabled/>'}</p>
+          <Input disabled/>
+        </div>
+      </div>
+
+
+      <div className='row'>
+        <div className="element">
+          <p>{'<Input helperText="Some interesting text" />'}</p>
+          <Input helperText="Some interesting text" />
+        </div>
+
+        <div className="element">
+          <p>{'<Input helperText="Some interesting text" error/>'}</p>
+          <Input helperText="Some interesting text" error/>
+        </div>
+      </div>
+
+      
+      <div className='row'>
+        <div className="element">
+          <p>{"<Input startIcon='email'/> "}</p>
+          <Input startIcon='email'/> 
+        </div>
+
+        <div className="element">
+          <p>{"<Input endIcon='call'/> "}</p>
+          <Input endIcon='call'/> 
+        </div>
+      </div>
+      
+
+      <div className='row'>
+        <div className="element">
+          <p>{"<Input value='text' />"}</p>
+          <Input value='text' /> 
+        </div>
+      </div>
+      
+
+      <div className='row'>
+        <div className="element">
+          <p>{"<Input size='sm' />"}</p>
+          <Input size='sm' />
+        </div>
+
+        <div className="element">
+          <p>{"<Input size='md' />"}</p>
+          <Input size='md' />
+        </div>
+      </div>
+      
+      <div className='row'>
+        <div className="element" style={{width:"100%"}}>
+          <p>{"<Input fullWidth />"}</p>
+          <Input fullWidth />
+        </div>
+      </div>
+      
+
+      <div className='row'>
+        <div className="element">
+          <p>{"<Input rows={4} />"}</p>
+          <Input rows={4} />
+        </div>
+      </div>
+      
+      
+      
       
     </div>
   )
